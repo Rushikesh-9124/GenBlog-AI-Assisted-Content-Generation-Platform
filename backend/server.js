49ed8df118eae6debd37948e9991612e01d5a73d
Rushikesh-9124 <rushikesh6281@gmail.com>
2025-08-11 12:32:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,14 @@ import 'dotenv/config'
 import express from 'express'
 const app = express();
 import cors from 'cors'
+app.options('*', cors());
 import router from './routes/routes.js';
 import connectDB from './config/db.js';
 
 connectDB()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+// app.use(cors({ origin: "*" }));
 app.use('/api/v1', router)
 
 app.get('/', (req, res)=>{
